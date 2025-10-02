d6d398dcf75743727d46f44bfa1cb62fba03d471
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-02 11:33:22 +0200

    fix(server.ts): changed PORT variable name to match .env

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
-// server.ts
 import { app } from "./src/app";
 
-const PORT = Number(process.env.APP_PORT) || 4000;
+const APP_PORT = Number(process.env.APP_PORT) || 4000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(APP_PORT, () => {
+  console.log(`✅ Server running on http://localhost:${APP_PORT}`);
 });
