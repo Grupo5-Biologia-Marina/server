99e40631c1b396117a6ba3e400ce2256353f521a
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-16 22:36:13 +0200

    docs(README): updated README w/ tests and created a new backup

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,19 +5,9 @@ import { registerValidation, loginValidation } from "../validators/authValidatio
 
 const router = Router();
 
-<<<<<<< HEAD
-// Registro
-router.post("/register", registerValidation, validationMiddleware, registerUser);
-
-// Login
-router.post("/login", loginValidation, validationMiddleware, loginUser);
-
-// POST /auth/logout → cerrar sesión
-=======
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
->>>>>>> fe6a680355f22f7da349a9f8fa702956e655c633
 router.post("/logout", (req, res) => {
   res.clearCookie("token"); 
   res.status(200).json({ success: true, message: "Sesión cerrada correctamente" });
